9b61153a8b669f802e1f431a179ef298f79868a1
Kevin <kevillanova@gmail.com>
2023-04-17 01:16:05 -0300

    Configuração de Rotas Edição do router e adição dos esboços das Views principais

---
+++ src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <img class="locaweb-logo" src="@/assets/logo.svg" alt="Locaweb">
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Login'
+});
+</script>
+
+<style scoped></style>
---
+++ src/views/Welcome.vue
@@ -0,0 +1,13 @@
+<template>
+  <img class="locaweb-logo" src="@/assets/logo.svg" alt="Locaweb">
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Welcome'
+});
+</script>
+
+<style scoped></style>
