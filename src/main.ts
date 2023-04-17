d5e8a89710951cc32f460307a93697024e54c4a5
Kevin <kevillanova@gmail.com>
2023-04-17 20:14:17 -0300

    Adição do Fontawesome

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import {store, key} from './store';
+import { store, key } from './store';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faCheck);
 
 createApp(App)
-.use(store, key)
-.use(router).mount('#app');
+    .use(store, key)
+    .use(router)
+    .component('Fa', FontAwesomeIcon)
+    .mount('#app');
