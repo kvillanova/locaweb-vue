4615866b10f42a88192b8978861d85eba28f1791
Kevin <kevillanova@gmail.com>
2023-04-17 20:15:58 -0300

    Adição de condicionais no Router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,31 +1,61 @@
+import { store } from '@/store';
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('@/views/Welcome.vue')
+    redirect: '/welcome'
+  },
+  {
+    path: '/welcome',
+    name: 'Welcome',
+    component: () => import('@/views/Welcome.vue'),
+    meta: { usuarioLogado: true }
   },
   {
     path: '/login',
     name: 'Login',
-    component: () => import('@/views/Login.vue')
+    component: () => import('@/views/Login.vue'),
+    meta: { usuarioLogado: false }
   },
   {
     path: '/cadastro',
     name: 'Cadastro',
-    component: () => import('@/views/Cadastro.vue')
+    component: () => import('@/views/Cadastro.vue'),
+    meta: {
+      usuarioLogado: false,
+      planoSelecionado: true
+    }
   },
   {
     path: '/selecionar-plano',
     name: 'Selecionar Plano',
-    component: () => import('@/views/SelecionarPlano.vue')
+    component: () => import('@/views/SelecionarPlano.vue'),
+    meta: {
+      usuarioLogado: false
+    }
   }
 ];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.usuarioLogado && !store.state.usuarioLogado) {
+    next("/login");
+  }
+  else if (!to.meta.usuarioLogado && store.state.usuarioLogado) {
+    next("/welcome");
+  }
+  else if (to.meta.planoSelecionado && !store.state.planoSelecionado) {
+    next("/selecionar-plano");
+  }
+  else {
+    next();
+  }
 });
 
 export default router;
