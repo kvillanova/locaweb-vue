dc9983272807b092cdc7f4a110219b0b733e5e11
Kevin <kevillanova@gmail.com>
2023-04-17 20:15:08 -0300

    Adição do tipo IUsuarioLogado

--- src/interfaces/IUsuario.ts
+++ src/interfaces/IUsuario.ts
@@ -1,10 +1,12 @@
 export default interface IUsuario {
-    id: number,
-    email: string,
-    username: string,
-    password: string,
-    name: {
-      firstname: string,
-      lastname: string
-    }
+  id: number,
+  email: string,
+  username: string,
+  password: string,
+  name: {
+    firstname: string,
+    lastname: string
+  }
 };
+
+export type IUsuarioLogado = Pick<IUsuario, 'email' | 'name'>;
